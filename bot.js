dcd07f560d7169b0989fe31f17b5a9b8214bd261
zvshka <39166475+zvshka@users.noreply.github.com>
2018-06-24 12:24:56 +0300

    1

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var prefix = '!';
 const roles = config.roles
 const CLEAR_MESSAGES = '!clear';
 
-client.login('NDQxMTQwOTYyNDY3NjQzMzk0.Dcr8OQ.Pku_q24YHqP8kFC4WgiZFlpmiSA');
+client.login('NDQxMTQwOTYyNDY3NjQzMzk0.Dcr8OQ.Pku_q24YHqP8kFC4WgiZFlpmiSA').listen(process.env.PORT || 5000);
 
 client.on("message", message => {
 function multicolors() {
